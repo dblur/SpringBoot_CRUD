c73340401804c7a8eacf1c4e1db291e24ff83717
dblur <dblur@ya.ru>
2021-01-07 18:26:11 +0700

    Initial commit

---
+++ src/main/java/com/dbur/SpringBootCRUD/model/User.java
@@ -0,0 +1,25 @@
+package com.dbur.SpringBootCRUD.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+}
